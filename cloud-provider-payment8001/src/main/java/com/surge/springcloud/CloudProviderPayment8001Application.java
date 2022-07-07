93e8a79267f2fbe624d259d354405999f4aba409
surgelee <10130970+surgelee@users.noreply.github.com>
2022-07-07 16:44:53 +0800

    创建订单模块

---
+++ cloud-provider-payment8001/src/main/java/com/surge/springcloud/CloudProviderPayment8001Application.java
@@ -0,0 +1,19 @@
+package com.surge.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @author : Surge Lee
+* @date : 2022/7/7 15:26
+* @description : 
+* @modyified By: 
+*/
+@SpringBootApplication
+public class CloudProviderPayment8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPayment8001Application.class, args);
+        System.out.println("启动成功");
+    }
+}
