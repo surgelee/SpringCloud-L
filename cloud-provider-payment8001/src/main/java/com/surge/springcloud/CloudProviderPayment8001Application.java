21c744447784714809bf467d185bedf59e1b35e8
surgelee <10130970+surgelee@users.noreply.github.com>
2022-07-08 13:13:18 +0800

    将80与8001加入到eureka单机版

--- cloud-provider-payment8001/src/main/java/com/surge/springcloud/CloudProviderPayment8001Application.java
+++ cloud-provider-payment8001/src/main/java/com/surge/springcloud/CloudProviderPayment8001Application.java
@@ -2,6 +2,7 @@ package com.surge.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
 * @author : Surge Lee
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 * @modyified By: 
 */
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudProviderPayment8001Application {
 
     public static void main(String[] args) {
