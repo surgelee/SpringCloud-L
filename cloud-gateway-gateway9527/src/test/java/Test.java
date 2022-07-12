abf9c362918a34c58e1ee17e26be949f0d26f2d2
surgelee <10130970+surgelee@users.noreply.github.com>
2022-07-12 13:14:05 +0800

    Gateway 路由断言Predicate的使用

---
+++ cloud-gateway-gateway9527/src/test/java/Test.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author : Surge Lee
+ * @date : 2022/7/12 13:01
+ * @description :
+ * @modyified By:
+ */
+public class Test {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
